287192057cded27cdfd9de749a4361ba5877c37d
Fucker <290358009@qq.com>
2016-07-11 14:51:42 +0800

    初始化

---
+++ MYWeiBo/classes/Main/WBTabBarViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WBTabBarViewController.swift
+//  MYWeiBo
+//
+//  Created by 杨绍鑫 on 16/7/11.
+//  Copyright © 2016年 Y. All rights reserved.
+//
+
+import UIKit
+
+class WBTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
