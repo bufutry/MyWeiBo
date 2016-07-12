52ba2269d92a0a9558408bf2c5324071560ef09c
Fucker <290358009@qq.com>
2016-07-12 10:58:45 +0800

    初始化tarbar

--- MYWeiBo/classes/Main/WBTabBarViewController.swift
+++ MYWeiBo/classes/Main/WBTabBarViewController.swift
@@ -12,7 +12,13 @@ class WBTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = UIColor.whiteColor()
+        tabBar.tintColor = UIColor.orangeColor()
+        addChildViewController("HomeTableViewController", image: "tabbar_home",title: "首页")
+        addChildViewController("MessageTableViewController",image: "tabbar_message_center",title: "消息")
+        addChildViewController("DiscoverTableViewController",image: "tabbar_discover",title: "广场")
+        addChildViewController("MessageTableViewController",image: "tabbar_profile",title: "我")
+        
         // Do any additional setup after loading the view.
     }
 
@@ -21,6 +27,22 @@ class WBTabBarViewController: UITabBarController {
         // Dispose of any resources that can be recreated.
     }
     
+    private func addChildViewController(childController: String,image:String,title:String) {
+
+        let name = NSBundle.mainBundle().infoDictionary!["CFBundleExecutable"] as! String
+        
+        let cls:AnyClass? = NSClassFromString(name + "."+childController)
+        print(cls)
+        let vcCls = cls as! UIViewController.Type
+        
+        let vc = vcCls.init()
+        vc.title = title
+        
+        let nav = UINavigationController.init(rootViewController: vc)
+        vc.tabBarItem.image = UIImage.init(named: image)
+        vc.tabBarItem.selectedImage = UIImage.init(named: image+"_highlighted")
+        addChildViewController(nav)
+    }
 
     /*
     // MARK: - Navigation
